eef210f88d34105c6515b7edfc94a3aa5c012b65
Luigi Venti <luigiventi@luigis-mbp.nhb-gb.ibmmobiledemo.com>
2017-07-31 10:35:38 +0200

    Added Client Application

---
+++ client/dist/out-tsc/environments/environment.prod.js
@@ -0,0 +1,6 @@
+export var environment = {
+    production: true,
+    apiHost: 'http://APPNAME-api.eu-gb.mybluemix.net/',
+    apiPrefix: 'api/v1/'
+};
+//# sourceMappingURL=/Users/luigiventi/Projects/node-boilerplate/node-boilerplate/client/src/environments/environment.prod.js.map
