4e48ccc36d5832e30474dcedfaf30bca345b7803
Sjoerd Schouten <sjoerd@sjoerds-mbp.groningen.nl.ibm.com>
2017-08-01 15:02:09 +0200

    overview component added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,14 +13,16 @@ import {ThingsComponent} from './components/things/things.component';
 
 import {ThingService} from './services/thing.service';
 import {AuthenticationService} from './services/authentication.service';
-import { HeaderComponent } from './components/header/header.component';
+import {HeaderComponent} from './components/header/header.component';
+import {OverviewComponent} from './components/overview/overview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ThingsComponent,
-    HeaderComponent
+    HeaderComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
