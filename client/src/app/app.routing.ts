6f52af7710610f50a7a219003bd7ba1e9e3c72aa
rickmollema <rick.mollema-cic.netherlands@ibm.com>
2017-08-03 17:45:09 +0200

    Steps to ecmrs, routing ecmrs, edit table

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -4,11 +4,13 @@ import {AuthGuard} from './guards/index';
 import {LoginComponent} from './components/login/login.component';
 import {ThingsComponent} from './components/things/things.component';
 import {OverviewComponent} from './components/overview/overview.component';
+import {EcmrsComponent} from './components/overview/ecmrs/ecmrs.component';
 
 const appRoutes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'things', component: ThingsComponent, canActivate: [AuthGuard]},
   {path: 'overview', component: OverviewComponent},
+  {path: 'ecmrs', component: EcmrsComponent},
 
   // otherwise redirect to login
   {path: '**', redirectTo: 'things'}
