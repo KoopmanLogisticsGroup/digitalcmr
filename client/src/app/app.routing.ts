c3f7bec8fb297944fd593a43ea98fcb899435798
Sjoerd Schouten <sjoerd@sjoerds-mbp.groningen.nl.ibm.com>
2017-08-04 09:24:40 +0200

    added routing for GoodComponent

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -5,12 +5,14 @@ import {LoginComponent} from './components/login/login.component';
 import {ThingsComponent} from './components/things/things.component';
 import {OverviewComponent} from './components/overview/overview.component';
 import {EcmrsComponent} from './components/overview/ecmrs/ecmrs.component';
+import {GoodsComponent} from "./components/ecmr-detail/goods/goods.component"
 
 const appRoutes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'things', component: ThingsComponent, canActivate: [AuthGuard]},
   {path: 'overview', component: OverviewComponent},
   {path: 'ecmrs', component: EcmrsComponent},
+  {path: 'goods', component: GoodsComponent},
 
   // otherwise redirect to login
   {path: '**', redirectTo: 'things'}
