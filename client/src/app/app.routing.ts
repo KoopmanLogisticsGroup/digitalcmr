cc5173209fe015543bac024d41c8e8037746f573
Sjoerd Schouten <sjoerd@sjoerds-mbp.groningen.nl.ibm.com>
2017-08-04 09:28:57 +0200

    deleted Goods routing replaced with eCMRDetailComponent

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -5,14 +5,14 @@ import {LoginComponent} from './components/login/login.component';
 import {ThingsComponent} from './components/things/things.component';
 import {OverviewComponent} from './components/overview/overview.component';
 import {EcmrsComponent} from './components/overview/ecmrs/ecmrs.component';
-import {GoodsComponent} from "./components/ecmr-detail/goods/goods.component"
+import {EcmrDetailComponent} from "./components/ecmr-detail/ecmr-detail.component"
 
 const appRoutes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'things', component: ThingsComponent, canActivate: [AuthGuard]},
   {path: 'overview', component: OverviewComponent},
   {path: 'ecmrs', component: EcmrsComponent},
-  {path: 'goods', component: GoodsComponent},
+  {path: 'ecmr-detail', component: EcmrDetailComponent},
 
   // otherwise redirect to login
   {path: '**', redirectTo: 'things'}
