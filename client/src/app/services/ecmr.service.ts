769bdc625e68ae42aa30c9fceee5d7a2d3232111
rickmollema <rick.mollema-cic.netherlands@ibm.com>
2017-08-21 15:36:56 +0200

    Added data-binding through ngmodel, added Modal from previous pull request and added functionality

--- client/src/app/services/ecmr.service.ts
+++ client/src/app/services/ecmr.service.ts
@@ -4,26 +4,37 @@ import 'rxjs/add/operator/map';
 
 import {Configuration} from '../app.constants';
 import {AuthenticationService} from './authentication.service';
+import {Ecmr} from '../classes/ecmr.model';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class EcmrService {
   private actionUrl: string;
   private headers: any;
 
-  public userRole: string;
-
   public constructor(private _http: Http,
                      private _configuration: Configuration,
                      private _authenticationService: AuthenticationService) {
     this.actionUrl = `${_configuration.composerHost}${_configuration.composerPrefix}ECMR/`;
     this.headers = _authenticationService.createAuthorizationHeader();
+    console.log(this.headers);
   }
 
   public getAllEcmrs(ecmrID: string) {
     const user: any = JSON.parse(localStorage.getItem('currentUser')).user;
-    this.userRole = JSON.parse(localStorage.getItem('currentUser')).user.role;
     return this._http
       .get(this.actionUrl + ecmrID, {headers: this.headers})
       .map(res => res.json());
+  }
+
+  public updateEcmr(ecmr: Ecmr) {
+    // return this._http.post(this.actionUrl, ecmr, {headers: this.headers})
+    //   .map(res => res.json())
+    //   .catch(this.handleErrorObservable);
+  }
+
+  private handleErrorObservable(error: Response | any) {
+    console.error(error.message || error);
+    return Observable.throw(error.message || error);
   }
 }
