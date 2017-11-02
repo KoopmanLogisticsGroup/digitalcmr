ee352993599c1a4be7cbe203bc5866bf2d712f95
rickmollema <rick.mollema-cic.netherlands@ibm.com>
2017-11-02 12:37:06 +0100

    Added api endpoint to get TransportOrders, added queries for transport orders

---
+++ server/src/api/v1/TransportOrderController.ts
@@ -0,0 +1,37 @@
+import {
+  Get,
+  JsonController,
+  Req,
+  UseAfter,
+  UseInterceptor,
+  Param,
+  UseBefore
+} from 'routing-controllers';
+import {ErrorHandlerMiddleware, ComposerInterceptor, UserAuthenticatorMiddleware} from '../../middleware';
+import {JSONWebToken} from '../../utils/authentication/JSONWebToken';
+import {TransactionHandler} from '../../blockchain/TransactionHandler';
+import {Identity} from '../../domain/Identity';
+import {Config} from '../../config/index';
+
+@JsonController('/transportOrder')
+@UseBefore(UserAuthenticatorMiddleware)
+@UseInterceptor(ComposerInterceptor)
+@UseAfter(ErrorHandlerMiddleware)
+export class TransportOrderController {
+  public constructor(private transactionHandler: TransactionHandler) {
+  }
+
+  @Get('/')
+  public async getAllTransportOrders(@Req() request: any): Promise<any> {
+    const identity: Identity = new JSONWebToken(request).getIdentity();
+
+    return await this.transactionHandler.executeQuery(identity, Config.settings.composer.profile, 'getAllTransportOrders');
+  }
+
+  @Get('/orderID/:orderID')
+  public async getTransportOrderByOrderID(@Param('orderID') orderID: string, @Req() request: any): Promise<any> {
+    const identity: Identity = new JSONWebToken(request).getIdentity();
+
+    return await this.transactionHandler.executeQuery(identity, Config.settings.composer.profile, 'getTransportOrderById', {id: orderID});
+  }
+}
