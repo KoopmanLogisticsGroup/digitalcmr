585659415d0932503e3d58661436c539e74698a3
Lars Steenhuis <Lars.Steenhuis-CIC.Netherlands@ibm.com>
2017-11-30 16:00:09 +0100

    fixed name in error

--- composer/bna/lib/ecmrRules.js
+++ composer/bna/lib/ecmrRules.js
@@ -205,7 +205,7 @@ function updateECMRStatusToCancelled(tx) {
  */
 function updateExpectedPickupWindow(tx) {
   if (tx.ecmr.status !== EcmrStatus.Created) {
-    throw new Error('[UpdateExpectedDeliveryWindow] Transaction is not valid. Attempting to set an ExpectedPickupWindow when status is not CREATED. Actual status: ' + tx.ecmr.status);
+    throw new Error('[UpdateExpectedPickupWindow] Transaction is not valid. Attempting to set an ExpectedPickupWindow when status is not CREATED. Actual status: ' + tx.ecmr.status);
   }
   tx.ecmr.loading.expectedWindow = tx.expectedWindow;
 
