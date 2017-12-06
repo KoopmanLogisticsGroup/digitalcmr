4dc0ac6ead232ffb83ceb3721e1828729b0a2cb5
rickmollema <rick.mollema-cic.netherlands@ibm.com>
2017-12-06 11:46:53 +0100

    Refactored client to update ECMRs

--- client/src/app/services/ecmr.service.ts
+++ client/src/app/services/ecmr.service.ts
@@ -5,11 +5,20 @@ import 'rxjs/add/operator/map';
 import {Configuration} from '../app.constants';
 import {AuthenticationService} from './authentication.service';
 import {Observable} from 'rxjs/Observable';
+import {Ecmr} from '../interfaces/ecmr.interface';
 
 @Injectable()
 export class EcmrService {
   private actionUrl: string;
   private headers: Headers;
+
+  public EcmrStatus = {
+    Created:            'CREATED',
+    Loaded:             'LOADED',
+    InTransit:          'IN_TRANSIT',
+    Delivered:          'DELIVERED',
+    ConfirmedDelivered: 'CONFIRMED_DELIVERED'
+  };
 
   public constructor(private _http: Http,
                      private _configuration: Configuration,
@@ -30,9 +39,20 @@ export class EcmrService {
       .map(res => res.json())
   }
 
-  public updateEcmr(ecmr: any): Observable<any> {
-    return this._http.put(this.actionUrl, ecmr, {headers: this.headers})
-      .map(res => res.json())
+  public updateEcmr(ecmr: Ecmr): Observable<any> {
+    if (ecmr.status === this.EcmrStatus.Created) {
+      return this._http.put(this.actionUrl + 'status/LOADED', ecmr, {headers: this.headers})
+        .map(res => res.json())
+    } else if (ecmr.status === this.EcmrStatus.Loaded) {
+      return this._http.put(this.actionUrl + 'status/IN_TRANSIT', ecmr, {headers: this.headers})
+        .map(res => res.json())
+    } else if (ecmr.status === this.EcmrStatus.InTransit) {
+      return this._http.put(this.actionUrl + 'status/DELIVERED', ecmr, {headers: this.headers})
+        .map(res => res.json())
+    } else if (ecmr.status === this.EcmrStatus.Delivered) {
+      return this._http.put(this.actionUrl + 'status/CONFIRMED_DELIVERED', ecmr, {headers: this.headers})
+        .map(res => res.json())
+    }
   }
 
   public getEcmrsByTransportOrderID(orderID: string): Observable<any> {
