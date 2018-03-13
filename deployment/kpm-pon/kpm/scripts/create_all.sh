3b89e66d323bd70d5f9453edc48f6c17f78c4b1d
Cesare V <cesare.valitutto@nl.ibm.com>
2018-03-13 18:01:51 +0100

    KPM-PON K8s initial configuration

--- deployment/kpm-pon/kpm/scripts/create_all.sh
+++ deployment/kpm-pon/kpm/scripts/create_all.sh
@@ -23,20 +23,3 @@ CHANNEL_NAME="channel1" PEER_MSPID="kpm-ponMSP" PEER_ADDRESS="blockchain-kpm-pon
 echo ""
 echo "=> CREATE_ALL: Creating composer playground"
 create/create_composer-playground.sh
-
-# Can't create this until the user has performed manual actions in the Composer Playground.
-# echo ""
-# echo "=> CREATE_ALL: Creating composer rest server"
-# create/create_composer-rest-server.sh
-
-#echo ""
-#echo "=> CREATE_ALL: Running Install Chaincode on kpm-pon Peer1"
-#CHAINCODE_NAME="example02" CHAINCODE_VERSION="v1" MSP_CONFIGPATH="/shared/crypto-config/peerOrganizations/kpm-pon/users/Admin@kpm-pon/msp"  PEER_MSPID="kpm-ponMSP" PEER_ADDRESS="blockchain-kpm-ponpeer:5010" create/chaincode_install.sh
-
-#echo ""
-#echo "=> CREATE_ALL: Running Install Chaincode on pon Peer1"
-#CHAINCODE_NAME="example02" CHAINCODE_VERSION="v1" MSP_CONFIGPATH="/shared/crypto-config/peerOrganizations/pon/users/Admin@pon/msp"  PEER_MSPID="ponMSP" PEER_ADDRESS="blockchain-ponpeer:5010" create/chaincode_install.sh
-#
-#echo ""
-#echo "=> CREATE_ALL: Running instantiate chaincode on channel \"channel1\" using \"kpm-ponMSP\""
-#CHANNEL_NAME="channel1" CHAINCODE_NAME="example02" CHAINCODE_VERSION="v1" MSP_CONFIGPATH="/shared/crypto-config/peerOrganizations/kpm-pon/users/Admin@kpm-pon/msp"  PEER_MSPID="kpm-ponMSP" PEER_ADDRESS="blockchain-kpm-ponpeer:5010" create/chaincode_instantiate.sh
