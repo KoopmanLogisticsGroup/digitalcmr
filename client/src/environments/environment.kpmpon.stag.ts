4bbc241ddd61bded0215afa08a33f925c089d604
Ewold Habing <EwoldHabing-ISC@nl.ibm.com>
2018-03-13 15:57:34 +0100

    add config for k8s and kpm-pon staging

---
+++ client/src/environments/environment.kpmpon.stag.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiHost:    'http://159.122.179.221:31001/',
+  apiPrefix:  'api/v1/'
+};
---
+++ server/src/config/kpm-pon-staging.config.ts
@@ -0,0 +1,20 @@
+import {Settings} from './settings.interface';
+
+export class KpmPonConfig {
+  public static get settings(): Settings {
+    return {
+      composer:     {
+        profile:   'kpm-pon',
+        network:   'digital-cmr-network',
+        channel:   'composerchannel',
+        namespace: 'org.digitalcmr'
+      },
+      host:         '0.0.0.0',
+      serverSecret: 'sUp4hS3cr37kE9c0D3',
+      privateDB:    {
+        host: 'privatedb-kpm-pon',
+        port: '5984'
+      }
+    };
+  }
+}
