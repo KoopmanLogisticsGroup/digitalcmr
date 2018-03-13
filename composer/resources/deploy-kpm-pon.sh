559c3206baff2f578975c3f07904bf0249dcb966
Cesare V <cesare.valitutto@nl.ibm.com>
2018-03-13 13:34:37 +0100

    OPEN ISSUE: Error trying deploy. Error: Failed to deserialize creator identity, err MSP ponMSP is unknown

--- composer/resources/deploy-kpm-pon.sh
+++ composer/resources/deploy-kpm-pon.sh
@@ -10,8 +10,8 @@ sleep 10
 
  composer network deploy -a "${COMPOSER_BUSINESS_NETWORK}".bna -i "${COMPOSER_ENROLLMENT_ID}" -s "${COMPOSER_ENROLLMENT_SECRET}" -p "kpm-pon"
 
-  composer identity import -u "${COMPOSER_ENROLLMENT_ID}" \
+  composer identity import -u "adminPon" \
  -p "pon" -c /admin-crypto/pon/signcerts/Admin@pon-cert.pem \
  -k /admin-crypto/pon/keystore/`ls /admin-crypto/pon/keystore`
 
- composer network deploy -a "${COMPOSER_BUSINESS_NETWORK}".bna -i "${COMPOSER_ENROLLMENT_ID}" -s "${COMPOSER_ENROLLMENT_SECRET}" -p "pon"
+ composer network deploy -a "${COMPOSER_BUSINESS_NETWORK}".bna -i "adminPon" -s "${COMPOSER_ENROLLMENT_SECRET}" -p "pon"
