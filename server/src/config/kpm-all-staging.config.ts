44ba23b361603d03a5bda5bfb8f910af6ac9f1f1
Luigi Venti <luigiventi@luigis-mbp-1.groningen.nl.ibm.com>
2018-05-01 14:24:24 +0200

    Add staging environment on server

---
+++ server/src/config/kpm-all-staging.config.ts
@@ -0,0 +1,18 @@
+import {Settings} from './settings.interface';
+
+export class KpmAllStagingConfig {
+  public static get settings(): Settings {
+    return {
+      composer:     {
+        profile:   'kpm-all-staging',
+        network:   'digital-cmr-network',
+        channel:   'composerchannel',
+        namespace: 'org.digitalcmr'
+      },
+      privateDB:    {
+        host: 'privatedb-kpm-all',
+        port: '5984'
+      }
+    };
+  }
+}
