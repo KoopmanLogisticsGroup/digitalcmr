44640a5d6b4772f130c087e2ad7d1f7b25f48114
Luigi Venti <luigiventi@luigis-mbp-1.groningen.nl.ibm.com>
2018-07-03 16:51:32 +0200

    Implement connection clean up Refactor connection pool as map Schedule cron job

--- server/src/connections/ConnectionPoolManager.ts
+++ server/src/connections/ConnectionPoolManager.ts
@@ -1,23 +1,65 @@
 import {Connection} from './entities/Connection';
+import {CronJob} from 'cron';
+import {isUndefined} from 'util';
+import {LoggerInstance} from 'winston';
+import {LoggerFactory} from '../utils/logger/LoggerFactory';
+import {Container} from 'typedi';
+
+export const connectionMaxAge            = 3600000;
+export const cronPatternCleanConnections = '*/20 * * * *'; //at every 20 minutes
 
 export class ConnectionPoolManager {
-  private connectionPool: { [key: string]: Connection } = {};
+  private logger: LoggerInstance;
+  private connectionPool: Map<string, Connection> = new Map<string, Connection>();
+  private cleanConnectionsJob: any;
+
+  public constructor() {
+    this.logger = Container.get(LoggerFactory).get('ConnectionPoolManager');
+
+    try {
+      this.cleanConnectionsJob = new CronJob(cronPatternCleanConnections, this.cleanConnections);
+    } catch (invalidCronJobPatternError) {
+      this.logger.error(cronPatternCleanConnections + ': invalid cron pattern; Job has not been registered');
+    }
+  }
 
   public addConnection(userID: string, connection: Connection): void {
-    if (!this.connectionPool[userID]) {
-      this.connectionPool[userID] = connection;
+    if (!this.connectionPool.get(userID)) {
+      this.connectionPool.set(userID, connection);
     }
   }
 
   public userHasConnection(userID: string): boolean {
-    return userID in this.connectionPool;
+    return this.connectionPool.has(userID);
   }
 
   public getConnection(userID: string): Connection {
-    if (!this.connectionPool[userID]) {
+    const connection = this.connectionPool.get(userID);
+
+    if (!connection) {
       throw new Error('No connection defined for this user');
     }
 
-    return this.connectionPool[userID];
+    if (this.isConnectionExpired(connection)) {
+      this.connectionPool.delete(userID);
+
+      throw new Error('Connection for ' + userID + ' has expired');
+    }
+
+    return connection;
+  }
+
+  private cleanConnections(): void {
+    for (let userID of this.connectionPool.keys()) {
+      let connection = this.connectionPool.get(userID);
+      if (this.isConnectionExpired(connection)) {
+        this.connectionPool.delete(userID);
+      }
+    }
+  }
+
+  private isConnectionExpired(connection?: Connection): boolean {
+    return !isUndefined(connection) &&
+      new Date().getTime() - connection.getCreatedTs() > connectionMaxAge;
   }
 }
--- server/src/connections/entities/Connection.ts
+++ server/src/connections/entities/Connection.ts
@@ -2,9 +2,12 @@ import {Factory} from 'composer-common';
 import {BusinessNetworkHandler} from '../../blockchain/BusinessNetworkHandler';
 
 export class Connection {
+  private createdTs: number;
+
   public constructor(private _businessNetworkHandler: BusinessNetworkHandler,
                      private _factory: Factory,
                      private _serializer: any) {
+    this.createdTs = new Date().getTime();
   }
 
   public get businessNetworkHandler(): BusinessNetworkHandler {
@@ -17,5 +20,9 @@ export class Connection {
 
   public get serializer(): any {
     return this._serializer;
+  }
+
+  public getCreatedTs(): number {
+    return this.createdTs;
   }
 }
