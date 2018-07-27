70b565641c8a26044a3754bacdb34f5ca870a57a
Luigi Venti <luigiventi@luigis-mbp-1.groningen.nl.ibm.com>
2018-07-27 15:19:06 +0200

    reformat

--- server/src/error/ErrorMessage.ts
+++ server/src/error/ErrorMessage.ts
@@ -2,7 +2,7 @@ export enum ErrorMessage {
   defaultErrorMessage                 = 'Internal server error',
   loginErrorMessage                   = 'An error occurred during login',
   loginInvalidCredentialsErrorMessage = 'Invalid credentials',
-  queryErrorMessage                          = 'An error occurred while executing the query',
-  invokeErrorMessage                         = 'An error occurred while invoking the transaction',
-  identityAddErrorMessage                    = 'An error occurred while adding a new identity'
+  queryErrorMessage                   = 'An error occurred while executing the query',
+  invokeErrorMessage                  = 'An error occurred while invoking the transaction',
+  identityAddErrorMessage             = 'An error occurred while adding a new identity'
 }
