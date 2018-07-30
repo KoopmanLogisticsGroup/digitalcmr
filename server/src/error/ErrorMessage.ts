1f53bccf9b88cc40e65d635f33cb74881eeb8a7a
Luigi Venti <luigiventi@luigis-mbp-1.groningen.nl.ibm.com>
2018-07-30 12:07:00 +0200

    Improve findOne error message

--- server/src/error/ErrorMessage.ts
+++ server/src/error/ErrorMessage.ts
@@ -3,7 +3,7 @@ export enum ErrorMessage {
   loginErrorMessage                   = 'An error occurred during login',
   loginInvalidCredentialsErrorMessage = 'Invalid credentials',
   queryErrorMessage                   = 'An error occurred while executing the query',
-  findOneErrorMessage                 = 'The asset with the specified ID could not be found',
+  findOneErrorMessage                 = 'The asset with the specified ID could not be found. It may not exist or user may not have the permission to access it',
   invokeErrorMessage                  = 'An error occurred while invoking the transaction',
   identityAddErrorMessage             = 'An error occurred while adding a new identity'
 }
