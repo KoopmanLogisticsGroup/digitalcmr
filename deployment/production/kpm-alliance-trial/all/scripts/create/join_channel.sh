06d5c24aa3d24886defa99110539b5054fc65c25
Ewold Habing <EwoldHabing-ISC@nl.ibm.com>
2018-07-31 14:33:20 +0200

    update ip-adresses of the trial scripts

--- deployment/production/kpm-alliance-trial/all/scripts/create/join_channel.sh
+++ deployment/production/kpm-alliance-trial/all/scripts/create/join_channel.sh
@@ -48,11 +48,11 @@ MSP_CONFIGPATH=${MSP_CONFIGPATH:-/fabric-config/Admin@all/msp}
 
 # Default to "orderer-kpm-all:31010" if not defined
 if [ -z "${ORDERER_ADDRESS}" ]; then
-	echo "ORDERER_ADDRESS not defined. I will use \"149.81.124.86:31010\"."
+	echo "ORDERER_ADDRESS not defined. I will use \"149.81.124.90:31010\"."
 	echo "I will wait 5 seconds before continuing."
 	sleep 5
 fi
-ORDERER_ADDRESS=${ORDERER_ADDRESS:-149.81.124.86:31010}
+ORDERER_ADDRESS=${ORDERER_ADDRESS:-149.81.124.90:31010}
 
 echo "Deleting old channel pods if exists"
 echo "Running: ${KUBECONFIG_FOLDER}/../scripts/delete/delete_channel-pods.sh"
