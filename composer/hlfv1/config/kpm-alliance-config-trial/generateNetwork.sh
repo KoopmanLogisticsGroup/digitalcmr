2e97dd2ba109433329f7dd28073fd87f0d4b3121
Luigi Venti <luigiventi@luigis-mbp-1.groningen.nl.ibm.com>
2018-08-16 11:38:45 +0200

    Update crypto CA files

--- composer/hlfv1/config/kpm-alliance-config-trial/generateNetwork.sh
+++ composer/hlfv1/config/kpm-alliance-config-trial/generateNetwork.sh
@@ -11,11 +11,11 @@ if [ ! -d "./channel-artifacts" ]; then
   mkdir ./channel-artifacts
 fi
 
-#echo "Download fabric-binary and extract them in ./bin directory"
-#curl -sSL https://goo.gl/kFFqh5 | bash -s $FABRIC_VERSION
+echo "Download fabric-binary and extract them in ./bin directory"
+curl -sSL https://goo.gl/kFFqh5 | bash -s $FABRIC_VERSION
 
-#echo "Generate certificates using cryptogen tool"
-#./bin/cryptogen generate --config=./crypto-config.yaml
+echo "Generate certificates using cryptogen tool"
+./bin/cryptogen generate --config=./crypto-config.yaml
 
 echo "Generating Orderer Genesis block"
 ./bin/configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ./$GENESIS.block
